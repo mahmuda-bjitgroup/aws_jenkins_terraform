ab82f1d622dde37bdd2eccc0a88bdf3d0eda5249
mahmuda-bjitgroup <142192979+mahmuda-bjitgroup@users.noreply.github.com>
2023-09-01 17:08:28 +0600

    Update provider.tf

--- provider.tf
+++ provider.tf
@@ -12,6 +12,6 @@ terraform {
 
 provider "aws" {
   # profile = "default"
-  shared_credentials_file = "/home/keya/.aws/credentials"
+   shared_credentials_files = ["/home/keya/.aws/credentials"]
   region  = "us-east-1"
 }
