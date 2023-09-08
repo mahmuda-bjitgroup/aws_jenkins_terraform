75621594245391d7a248caf2689612be69ef8e74
mahmuda-bjitgroup <142192979+mahmuda-bjitgroup@users.noreply.github.com>
2023-09-08 11:15:12 +0600

    Update ec2.tf

--- ec2.tf
+++ ec2.tf
@@ -13,3 +13,13 @@ resource "aws_instance" "tf_ec2" {
   }
 
 }
+
+resource "random_password" "password" {
+  length           = 16
+  special          = true
+  override_special = "!#$%&*()-_=+[]{}<>:?"
+}
+
+variable "cidr" {
+  default = "172.16.0.0/20"
+}
